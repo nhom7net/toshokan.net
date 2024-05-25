e43478e28ff005eb1f40cf03ae096364abbac6af
lemaiductin <trungtin20102003@gmail.com>
2024-05-25 09:55:06 +0700

    created models

---
+++ Models/Librarian.cs
@@ -0,0 +1,13 @@
+﻿namespace toshokan.Models
+{
+    public class Librarian
+    {
+        public int LibrarianID {  get; set; }
+        public string FirstName {  get; set; }
+        public string LastName { get; set; } 
+        public DateTime EmploymentDate { get; set; }
+        public string PhoneNumber {  get; set; }
+        public string Email { get; set; }
+
+    }
+}
---
+++ Models/Member.cs
@@ -0,0 +1,15 @@
+﻿namespace toshokan.Models
+{
+    public class Member
+    {
+        public int MemberID { get; set; }
+        public string FirstName {  get; set; }
+        public string LastName { get; set; }
+        public DateTime MembershipDate { get; set; }
+        public string Address {  get; set; }
+        public string PhoneNumber {  get; set; }
+        public string Email { get; set; }
+        ICollection<Loan>Loans { get; set; }
+        ICollection<Reservation> Reservations {  get; set; }    
+    }
+}
