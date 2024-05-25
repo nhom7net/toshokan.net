434f5821ad5d4295597e7bbff05edde86d04168c
lemaiductin <146379514+lemaiductin@users.noreply.github.com>
2024-05-25 10:33:20 +0700

    Created Models (#2)
    * Created models
    * Update .gitignore

---
+++ Models/Loan.cs
@@ -0,0 +1,15 @@
+﻿namespace toshokan.Models
+{
+    public class Loan
+    {
+        public int LoanID { get; set; } 
+        public int BookID {  get; set; }
+        public int MemberID {  get; set; }
+        public DateTime LoanDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+        public bool Returned {  get; set; }
+        public Book Book { get; set; }
+        public Member Member { get; set; } 
+
+    }
+}
