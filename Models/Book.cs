6f3ac6d64bf126d7cf632810c9296d17bc396f52
lemaiductin <trungtin20102003@gmail.com>
2024-05-25 10:28:58 +0700

    Created models

---
+++ Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace toshokan.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author {  get; set; }
+        public string Genre {  get; set; }
+        public int CopiesAvailable {  get; set; }
+        public DateTime PublishedDate {  get; set; }
+        public ICollection<Loan> Loans { get; set; }
+    }
+}
---
+++ Models/Reservation.cs
@@ -0,0 +1,14 @@
+﻿namespace toshokan.Models
+{
+    public class Reservation
+    {
+        public int ReservationID { get; set; }
+        public int BookID {  get; set; }
+        public int MemberID {  get; set; }
+        public DateTime ReservationDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public string Status {  get; set; }
+        public Book Book { get; set; }  
+        public Member Member { get; set; }
+    }
+}
