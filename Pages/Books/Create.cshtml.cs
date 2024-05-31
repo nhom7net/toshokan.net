5f225a0c9b29d28fd0b608b7c156ed815c281566
Pham Van Dong <0769408945z@gmail.com>
2024-05-31 23:47:58 +0700

    CRUD scaffold

---
+++ Pages/Books/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using toshokan.Data;
+using toshokan.Models;
+
+namespace toshokan.Pages.Books
+{
+    public class CreateModel : PageModel
+    {
+        private readonly toshokan.Data.toshokanContext _context;
+
+        public CreateModel(toshokan.Data.toshokanContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Book Book { get; set; } = default!;
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Book.Add(Book);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Pages/Loans/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using toshokan.Data;
+using toshokan.Models;
+
+namespace toshokan.Pages.Loans
+{
+    public class IndexModel : PageModel
+    {
+        private readonly toshokan.Data.toshokanContext _context;
+
+        public IndexModel(toshokan.Data.toshokanContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Loan> Loan { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Loan = await _context.Loan
+                .Include(l => l.Book)
+                .Include(l => l.Member).ToListAsync();
+        }
+    }
+}
