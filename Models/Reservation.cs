225d34636b2a577739d75925c9d9d1041d2e51d5
Sora <34794115+sorae42@users.noreply.github.com>
2024-06-03 13:19:02 +0700

    fix: more reasonable models

--- Models/Reservation.cs
+++ Models/Reservation.cs
@@ -3,8 +3,6 @@
     public class Reservation
     {
         public int ReservationID { get; set; }
-        public int BookID {  get; set; }
-        public int MemberID {  get; set; }
         public DateTime ReservationDate { get; set; }
         public DateTime ExpirationDate { get; set; }
         public string Status {  get; set; }
