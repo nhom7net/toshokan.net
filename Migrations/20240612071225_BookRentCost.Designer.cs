68e3e53bbfa86c41da5120261f2e5bbca7316061
Jin-Tankuji <nhoanghai2003@gmail.com>
2024-06-12 21:00:36 +0700

    Thêm thuộc tính

--- Migrations/20240612071225_BookRentCost.Designer.cs
+++ Migrations/20240612071225_BookRentCost.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using toshokan.Data;
 
@@ -10,9 +11,11 @@ using toshokan.Data;
 namespace toshokan.Migrations
 {
     [DbContext(typeof(toshokanContext))]
-    partial class toshokanContextModelSnapshot : ModelSnapshot
+    [Migration("20240612071225_BookRentCost")]
+    partial class BookRentCost
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.6");
