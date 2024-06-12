b352f5722bee8c3eea93232a4147aa54e198bb17
Jin-Tankuji <nhoanghai2003@gmail.com>
2024-06-12 21:01:25 +0700

    Thêm thuộc tính

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,12 +1,35 @@
-﻿namespace toshokan.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace toshokan.Models
 {
     public class Book
     {
+
         public int Id { get; set; }
+
+        [StringLength(200, MinimumLength = 3), Required]
         public string Title { get; set; }
+
+        [StringLength(60, MinimumLength = 3), Required]
         public string Author { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
         public string Genre { get; set; }
+
+        [Range(0, int.MaxValue)]
         public int CopiesAvailable { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
         public DateTime PublishedDate { get; set; }
+
+        public string ImgURL { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        [Required]
+        public decimal RentCost { get; set; }
     }
 }
