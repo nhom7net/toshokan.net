6d687e9f621a8f37cab96a1bb95f66d4ed41404d
Jin-Tankuji <nhoanghai2003@gmail.com>
2024-06-13 22:57:43 +0700

    Xử lý nút rent book

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function rentBookConfirm(bookId) {
+    window.location.href = 'https://localhost:7095/Books/Details?id=' + bookId;
+}
+
