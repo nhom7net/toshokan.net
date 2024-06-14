33784661667bef5ce8eb39293ed8a4fcf851f322
Pham Van Dong <0769408945z@gmail.com>
2024-06-14 23:05:49 +0700

    bỏ 1 số ràng buộc

--- Models/Member.cs
+++ Models/Member.cs
@@ -8,37 +8,26 @@ namespace toshokan.Models
         [Key]
         public int MemberID { get; set; }
 
-        [Required(ErrorMessage = "First name is required.")]
-        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         public string FirstName { get; set; }
-
-        [Required(ErrorMessage = "Last name is required.")]
-        [StringLength(50, ErrorMessage = "Last name cannot be longer than 50 characters.")]
         public string LastName { get; set; }
-
-        [Required(ErrorMessage = "Address is required.")]
-        [StringLength(100, ErrorMessage = "Address cannot be longer than 100 characters.")]
         public string Address { get; set; }
 
-        [Required(ErrorMessage = "Phone number is required.")]
         [Phone(ErrorMessage = "Invalid phone number format.")]
         [StringLength(15, ErrorMessage = "Phone number cannot be longer than 15 characters.")]
         public string PhoneNumber { get; set; }
 
-        [Required(ErrorMessage = "Email is required.")]
         [EmailAddress(ErrorMessage = "Invalid email address format.")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Membership date is required.")]
         [DataType(DataType.Date)]
         public DateTime MembershipDate { get; set; }
 
-        [Required(ErrorMessage = "Username is required.")]
+
         [StringLength(30, ErrorMessage = "Username cannot be longer than 30 characters.")]
         public string Username { get; set; }
 
-        [Required(ErrorMessage = "Password is required.")]
-        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
+    
+      
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
