e0260f98614955bbc1ee5e895c0831d682e9b75a
Pham Van Dong <0769408945z@gmail.com>
2024-06-14 23:05:49 +0700

    bỏ 1 số ràng buộc

--- Models/Librarian.cs
+++ Models/Librarian.cs
@@ -7,32 +7,26 @@ namespace toshokan.Models
         [Key]
         public int LibrarianID { get; set; }
 
-        [Required(ErrorMessage = "First name is required")]
         [StringLength(50, ErrorMessage = "First name can't be longer than 50 characters")]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "Last name is required")]
+ 
         [StringLength(50, ErrorMessage = "Last name can't be longer than 50 characters")]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "Phone number is required")]
         [Phone(ErrorMessage = "Invalid phone number")]
         public string PhoneNumber { get; set; }
 
-        [Required(ErrorMessage = "Email is required")]
         [EmailAddress(ErrorMessage = "Invalid email address")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Employment date is required")]
         [DataType(DataType.Date)]
         public DateTime EmploymentDate { get; set; }
 
-        [Required(ErrorMessage = "Username is required")]
+
         [StringLength(20, ErrorMessage = "Username can't be longer than 20 characters")]
         public string Username { get; set; }
 
-        [Required(ErrorMessage = "Password is required")]
-        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
