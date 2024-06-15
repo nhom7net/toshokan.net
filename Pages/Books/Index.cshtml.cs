b44c5c316ce8cf3ea2af9e76a331d18e44288e3f
Jin-Tankuji <nhoanghai2003@gmail.com>
2024-06-15 11:23:53 +0700

    Thêm search, filter trong model book

--- Pages/Books/Index.cshtml.cs
+++ Pages/Books/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using toshokan.Data;
 using toshokan.Models;
@@ -21,9 +22,34 @@ namespace toshokan.Pages.Books
 
         public IList<Book> Book { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Genres { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? BookGenre { get; set; }
+
         public async Task OnGetAsync()
         {
-            Book = await _context.Book.ToListAsync();
+            IQueryable<string> genreQuery = from m in _context.Book
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var books = from m in _context.Book
+                        select m;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                books = books.Where(s => s.Title.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(BookGenre))
+            {
+                books = books.Where(x => x.Genre.Contains(BookGenre));
+            }
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Book = await books.ToListAsync();
         }
     }
 }
