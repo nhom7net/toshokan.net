2d9120f4b17fb0782fe65fcc43dc87805a1f0879
Jin-Tankuji <nhoanghai2003@gmail.com>
2024-06-15 11:18:08 +0700

    Thêm tìm kiếm và lọc

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,6 +4,8 @@ using toshokan.Models;
 using toshokan.Data;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
 
 
 namespace toshokan.Pages;
@@ -19,8 +21,32 @@ public class IndexModel : PageModel
 
     public IList<Book> Books { get; set; }
 
+    [BindProperty(SupportsGet = true)]
+    public string? SearchString { get; set; }
+    public SelectList? Genres { get; set; }
+
+    [BindProperty(SupportsGet = true)]
+    public string? BookGenre { get; set; }
+
     public async Task OnGetAsync()
     {
-        Books = await _context.Book.ToListAsync();
+        IQueryable<string> genreQuery = from m in _context.Book
+                                        orderby m.Genre
+                                        select m.Genre;
+
+        var books = from m in _context.Book
+                    select m;
+
+        if (!string.IsNullOrEmpty(SearchString))
+        {
+            books = books.Where(s => s.Title.Contains(SearchString));
+        }
+
+        if (!string.IsNullOrEmpty(BookGenre))
+        {
+            books = books.Where(x => x.Genre.Contains(BookGenre));
+        }
+        Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+        Books = await books.ToListAsync();
     }
 }
